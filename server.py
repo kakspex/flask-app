fd0b3e40455007fea1bce762268c2e50621981a0
kakspex <168642755+kakspex@users.noreply.github.com>
2024-11-30 17:11:52 +0100

    Update server.py

--- server.py
+++ server.py
@@ -86,4 +86,4 @@ def get_result(task_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
